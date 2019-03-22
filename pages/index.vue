013024a830c4c29282b7afb001836ddc26c27a63
Yolanda Obando <iryolob@gmail.com>
2019-03-21 20:28:53 -0500

    iniciando proyecto

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>HOLA {{msg}}</h1>
+    <input type="text" v-model="msg">
+    <button @click="guardar">Guardar</button>
+    <table>
+      <tr>
+        <th>nombre</th>
+      </tr>
+      <tr v-for="(item, index) in municipio" :key="index">
+          <td>
+              {{item}}
+          </td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "Yolanda Obando",
+      municipio: ["mocoa", "villagarzon", "caicedo"]
+    };
+  },
+  methods:{
+      guardar(){
+         // alert("guardando....#)
+
+         this.municipio.push(this.msg)
+         this.msg=''
+
+      }
+  }
+};
+</script>
